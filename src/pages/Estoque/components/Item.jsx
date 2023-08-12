34f527d76aed397d998f8bbead37a011cb6e7fc8
Samarión Júnior <samarion2@hotmail.com>
2023-08-12 17:06:04 -0300

    Atualização 15

--- src/pages/Estoque/components/Item.jsx
+++ src/pages/Estoque/components/Item.jsx
@@ -21,28 +21,34 @@ const Item = ({item}) => {
 
     const change = (callback, tipo, value) => isEdicaoHabilitada ? callback(tipo(value)) : null
 
+    function adicinarEfeitoDeEdicao(status, id){
+        const aux = document.getElementsByClassName(`ItemBodyRowInput${id}`)
+        for(let i = 0; i < aux.length; i++){
+            status ? aux[i].style.borderBottom = "3px solid black": aux[i].style.borderBottom = "3px solid transparent"
+        }
+    }
+
     return (
         <div className='Item ItemCollumn'>
             <ImageSmall/>
-
             <div className="ItemBodyRow ItemBodyEstoque">
                 <div className="ItemBodyRowDescription ItemBodyEstoqueDescription">
-                    <input className='ItemBodyRowInput ItemBodyRowInputTitle' type="text" value={nome} onChange={(e) => change(setNome, String, e.target.value)}/>
-                    <input className='ItemBodyRowInput ItemBodyRowInputPreco' type="number" step="0.01" value={preco} onChange={(e) => change(setPreco, Number, e.target.value)}/>
-                    <input className='ItemBodyRowInput ItemBodyRowInputQuantidade' type="number" step="0.01" value={quantidade} onChange={(e) => change(setQuantidade, Number, e.target.value)}/>
+                    <input className={`ItemBodyRowInput ItemBodyRowInputTitle ItemBodyRowInput${id}`} type="text" value={nome} onChange={(e) => change(setNome, String, e.target.value)}/>
+                    <input className={`ItemBodyRowInput ItemBodyRowInputPreco ItemBodyRowInput${id}`} type="number" step="0.01" value={preco} onChange={(e) => change(setPreco, Number, e.target.value)}/>
+                    <input className={`ItemBodyRowInput ItemBodyRowInputQuantidade ItemBodyRowInput${id}`} type="number" step="0.01" value={quantidade} onChange={(e) => change(setQuantidade, Number, e.target.value)}/>
                 </div>
                 <div className='ItemBodyRowActions ItemBodyEstoqueActions'>
                     {!isEdicaoHabilitada ? 
                         <>
-                            <button className='ItemEstoque-Button' onClick={(e) => {setIsEdicaoHabilitada(true)}}>Habilitar Edição</button>
-                            <button className='ItemEstoque-Button' disabled>Salvar Edição</button>
-                            <button className='ItemEstoque-Button' disabled>Apagar</button>
+                            <button className='ItemEstoque-Button' onClick={(e) => {setIsEdicaoHabilitada(true); adicinarEfeitoDeEdicao(true, id)}}>Hab</button>
+                            <button className='ItemEstoque-Button' disabled>Sal</button>
+                            <button className='ItemEstoque-Button' disabled>Apa</button>
                         </>
                             : 
                         <>
-                            <button className='ItemEstoque-Button' onClick={(e) => {setIsEdicaoHabilitada(false); clear() }}>Cancelar Edição</button>
-                            <button className='ItemEstoque-Button' onClick={(e) => {dispatch(ListActions.setProdutosById(id, nome, quantidade, preco))}}>Salvar Edição</button>
-                            <button className='ItemEstoque-Button' onClick={(e) => {dispatch(ListActions.deleteProdutoById(id));}}>Apagar</button>
+                            <button className='ItemEstoque-Button' onClick={(e) => {setIsEdicaoHabilitada(false); clear(); adicinarEfeitoDeEdicao(false, id)}}>Can</button>
+                            <button className='ItemEstoque-Button' onClick={(e) => {dispatch(ListActions.setProdutosById(id, nome, quantidade, preco))}}>Sal</button>
+                            <button className='ItemEstoque-Button' onClick={(e) => {dispatch(ListActions.deleteProdutoById(id));}}>Apa</button>
                         </>
                     }
                 </div>
