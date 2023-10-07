0e52a72ccb0e1b14c130d5bd1b0010a35f854aef
Samarión Júnior <samarion2@hotmail.com>
2023-10-07 13:07:36 -0300

    atualizando o menu

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -4,17 +4,24 @@ import React from 'react';
 
 import { Link } from "react-router-dom";
 
+import productPNG from "../assets/product.jpg";
+import shoppingCarPNG from "../assets/shoppingCar.png";
+import paymentAreaPNG from "../assets/paymentArea.png";
+import ordersPNG from "../assets/orders.png";
+import panelPNG from "../assets/panel.png";
+import stockPNG from "../assets/stock.png";
+
 const Links = () => {
 
   return (
     <div className="Header">
       <div className='Links'>
-          <Link className='Link' to="/matheus/Produtos">P</Link>
-          <Link className='Link' to="/matheus/Carrinho">C</Link>
-          <Link className='Link' to="/matheus/Pagamento">P</Link>
-          <Link className='Link' to="/matheus/Pedidos">P</Link>
-          <Link className='Link' to="/matheus/Estoque">E</Link>
-          <Link className='Link' to="/matheus/Painel">P</Link>
+          <Link className='Link' to="/matheus/Produtos"><img src={productPNG} className="ImagemLink"/></Link>
+          <Link className='Link' to="/matheus/Carrinho"><img src={shoppingCarPNG} className="ImagemLink"/></Link>
+          <Link className='Link' to="/matheus/Pagamento"><img src={paymentAreaPNG} className="ImagemLink"/></Link>
+          <Link className='Link' to="/matheus/Pedidos"><img src={ordersPNG} className="ImagemLink"/></Link>
+          <Link className='Link' to="/matheus/Estoque"><img src={panelPNG} className="ImagemLink"/></Link>
+          <Link className='Link' to="/matheus/Painel"><img src={stockPNG} className="ImagemLink"/></Link>
           {/* <Link className='Link' to="/Produtos">Produtos</Link>
           <Link className='Link' to="/Carrinho">Carrinho</Link>
           <Link className='Link' to="/Pagamento">Pagamento</Link>
