d14857c52995d8f4a0d7602120fe42f377cd9fab
Samarión Júnior <samarion2@hotmail.com>
2023-10-21 19:06:06 -0300

    Atualização do css

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -4,13 +4,6 @@ import React from 'react';
 
 import { Link } from "react-router-dom";
 
-// import productPNG     from "../assets/image/link/product.jpg";
-// import shoppingCarPNG from "../assets/image/link/shoppingCar.png";
-// import paymentAreaPNG from "../assets/image/link/paymentArea.png";
-// import ordersPNG      from "../assets/image/link/orders.png";
-// import panelPNG       from "../assets/image/link/panel.png";
-// import stockPNG       from "../assets/image/link/stock.png";
-
 const Links = () => {
 
   return (
@@ -18,37 +11,33 @@ const Links = () => {
       <div className='Links'>
           <Link className='Link' to="/matheus/Produtos">
             <i className="fa-solid fa-house LinkIcon"/>
-            {/* <img src={productPNG} className="ImagemLink" alt=''/> */}
           </Link>
           <Link className='Link' to="/matheus/Carrinho">
             <i className="fa-solid fa-cart-shopping LinkIcon"/>
-            {/* <img src={shoppingCarPNG} className="ImagemLink" alt=''/> */}
           </Link>
           <Link className='Link' to="/matheus/Pagamento">
             <i className="fa-solid fa-credit-card LinkIcon"/>
-            {/* <img src={paymentAreaPNG} className="ImagemLink" alt=''/> */}
           </Link>
           <Link className='Link' to="/matheus/Pedidos">
             <i className="fa-solid fa-bag-shopping LinkIcon"/>
-            {/* <img src={ordersPNG} className="ImagemLink" alt=''/> */}
           </Link>
           <Link className='Link' to="/matheus/Estoque">
             <i className="fa-solid fa-gear LinkIcon"/>
-            {/* <img src={panelPNG} className="ImagemLink" alt=''/> */}
           </Link>
           <Link className='Link' to="/matheus/Painel">
             <i className="fa-solid fa-box LinkIcon"/>
-            {/* <img src={stockPNG} className="ImagemLink" alt=''/> */}
           </Link>
-          {/* <Link className='Link' to="/Produtos">Produtos</Link>
-          <Link className='Link' to="/Carrinho">Carrinho</Link>
-          <Link className='Link' to="/Pagamento">Pagamento</Link>
-          <Link className='Link' to="/Pedidos">Pedidos</Link>
-          <Link className='Link' to="/Estoque">Estoque</Link>
-          <Link className='Link' to="/Painel">Painel</Link> */}
       </div>
     </div>
   );
 }
 
-export default (Links)
+export default Links
+
+
+// {/* <Link className='Link' to="/Produtos">Produtos</Link>
+// <Link className='Link' to="/Carrinho">Carrinho</Link>
+// <Link className='Link' to="/Pagamento">Pagamento</Link>
+// <Link className='Link' to="/Pedidos">Pedidos</Link>
+// <Link className='Link' to="/Estoque">Estoque</Link>
+// <Link className='Link' to="/Painel">Painel</Link> */}
