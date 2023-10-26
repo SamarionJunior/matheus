e60e557747f2120f717e66179844fe0ee8acfe03
Samarión Júnior <samarion2@hotmail.com>
2023-10-26 18:27:19 -0300

    uptade for deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const Api = {
-    url: "http://localhost:3030",
-    // url: "https://matheus-back-end.vercel.app"
+    // url: "http://localhost:3030",
+    url: "https://matheus-back-end.vercel.app"
 }
